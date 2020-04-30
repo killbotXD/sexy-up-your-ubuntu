74603f2952ea4eca26d486be316a91e4dc9eaa5b
Soham <42870378+killbotXD@users.noreply.github.com>
2020-04-30 15:20:03 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+sudo apt update && sudo apt upgrade
+
+sudo add-apt-repository ppa:numix/ppa
+
+sudo add-apt-repository ppa:papirus/papirus
+
+sudo add-apt-repository ppa:snwh/pulp
+
+sudo add-apt-repository ppa:daniruiz/flat-remix
+
+sudo apt update
+
+sudo apt install numix-icon-theme-circle papirus-icon-theme paper-icon-theme flat-remixsudo apt install git gnome-tweak-tool chrome-gnome-shell
+
+git clone https://github.com/tliron/install-gnome-themes
+
+cd install-gnome-themes
+
+sudo ./install-requirements-debian
+
+./install-gnome-themes
